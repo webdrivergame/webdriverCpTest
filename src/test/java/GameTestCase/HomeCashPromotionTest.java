027a45f8b4953237c0318309fafa09b2cb9a0527
webdrivergame <jiang152849152849@gmail.com>
2019-02-12 16:31:57 +0700

    用例更新

--- src/test/java/GameTestCase/HomeCashPromotionTest.java
+++ src/test/java/GameTestCase/HomeCashPromotionTest.java
@@ -7,54 +7,53 @@ import org.testng.annotations.Test;
 
 
 
-
 public class HomeCashPromotionTest {
 
+   private WebDriver driver;
 
-    private WebDriver driver;
 
     @Test
-    public void ServiceFeeIncome() throws InterruptedException {
-        WebDriverUtil driverUtil2 = new WebDriverUtil(driver);
-        driverUtil2.LoginBefore();
-        driverUtil2.adminlogin("xiaochaoadmin", "123123");
-        driverUtil2.findElementByXpathAndClick("//*[@id=\"login_submit\"]");
+    public void TodaySystemWinOrLose() throws InterruptedException {
+        WebDriverUtil driverUtil1 = new WebDriverUtil(driver);
+        driverUtil1.LoginBefore();
+        driverUtil1.adminlogin("xiaochaoadmin", "123123");
+        driverUtil1.findElementByXpathAndClick("//*[@id=\"login_submit\"]");
         Thread.sleep(4000);
 
-        //判断登录成功
-        String adminText = driverUtil2.getTextByXpath("//*[@id=\"root\"]/section/header/div/div[2]/div/div[1]/div[2]/div/p");
-        Assert.assertEquals(adminText, "管理员小超");
+        //判断首页现金今日系统盈亏金额与跳转玩家输赢总计金额是否一致
+        String TodaySystemWinOrLose = driverUtil1.getTextByXpath("//*[@id=\"root\"]/section/section/main/div[2]/div[1]/div[2]/div[2]/div[1]/div/div/div/div[1]/div[2]/div[2]/span/span[3]");
+        driverUtil1.findElementByXpathAndClick("//*[@id=\"root\"]/section/section/main/div[2]/div[1]/div[2]/div[2]/div[1]/div/div/div/div[1]/div[2]/div[2]/span/span[3]");
+        String TodaySystemWinOrLoseJump = driverUtil1.getTextByXpath("//*[@id=\"root\"]/section/section/main/div[2]/div[2]/div/div/div/div[2]/div[1]/div[4]/li/span[2]");
 
-
-        //判断首页无限金币今日服务费收入和跳转金额是否一致
-        String ServiceFeeIncomeToday = driverUtil2.getTextByXpath("//*[@id=\"root\"]/section/section/main/div[2]/div[1]/div[1]/div[2]/div[1]/div/div/div/div[1]/div[2]/div[2]/span/span[3]");
-        driverUtil2.findElementByXpathAndClick("//*[@id=\"root\"]/section/section/main/div[2]/div[1]/div[1]/div[2]/div[1]/div/div/div/div[1]/div[2]/div[2]/span/span[3]");
-        String ServiceFeeIncomeTodayJump = driverUtil2.getTextByXpath("//*[@id=\"root\"]/section/section/main/div[2]/div[2]/div/div[1]/div/div[2]/div[1]/div[3]/span[2]");
-
-        Assert.assertEquals(ServiceFeeIncomeToday,ServiceFeeIncomeTodayJump);
-        driverUtil2.LoginAfter();
-    }
-
-    @Test
-    public void SinceBusinessOperationsTotalArrears() throws InterruptedException {
-        WebDriverUtil driverUtil4 = new WebDriverUtil(driver);
-        driverUtil4.LoginBefore();
-        driverUtil4.adminlogin("xiaochaoadmin","123123");
-        driverUtil4.findElementByXpathAndClick("//*[@id=\"login_submit\"]");
-        Thread.sleep(4000);
-
-
-        //判断无限金币自运营商户欠款总计与跳转金额是否一致
-        String SinceBusinessOperationsTotalArrears = driverUtil4.getTextByXpath("//*[@id=\"root\"]/section/section/main/div[2]/div[1]/div[1]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[2]/span/span[3]");
-        driverUtil4.findElementByXpathAndClick("//*[@id=\"root\"]/section/section/main/div[2]/div[1]/div[1]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[2]/span/span[3]");
-        Thread.sleep(3000);
-        driverUtil4.refreshpage();
-        String SinceBusinessOperationsTotalArrearsJump = driverUtil4.getTextByXpath("//*[@id=\"root\"]/section/section/main/div[2]/div[2]/div/div[1]/div/div[2]/div[1]/div/li/span[2]");
         try {
-            Assert.assertEquals(SinceBusinessOperationsTotalArrears,SinceBusinessOperationsTotalArrearsJump);
+            Assert.assertEquals(TodaySystemWinOrLose,TodaySystemWinOrLoseJump);
         }catch (AssertionError error){
-            System.out.println("自运营商户欠款金额与跳转金额不一致");
+            System.out.println("今日系统盈亏金额与跳转页面金额不相同");
+
         }
-        driverUtil4.LoginAfter();
+        driverUtil1.LoginAfter();
     }
+
+    @Test
+    public void TodayGenerateCommission() throws InterruptedException {
+        WebDriverUtil driverUtil3 = new WebDriverUtil(driver);
+        driverUtil3.LoginBefore();
+        driverUtil3.adminlogin("xiaochaoadmin","123123");
+        driverUtil3.findElementByXpathAndClick("//*[@id=\"login_submit\"]");
+        Thread.sleep(4000);
+
+        //判断首页今日产生佣金金额与跳转下级佣金统计页面今日佣金统计字段金额是否一致
+        String TodayGenerateCommission = driverUtil3.getTextByXpath("//*[@id=\"root\"]/section/section/main/div[2]/div[1]/div[2]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[2]/span/span[3]");
+        driverUtil3.findElementByXpathAndClick("//*[@id=\"root\"]/section/section/main/div[2]/div[1]/div[2]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[2]/span/span[3]");
+        String TodayGenerateCommissionjump = driverUtil3.getTextByXpath("//*[@id=\"root\"]/section/section/main/div[2]/div[2]/div/div[1]/div/div[2]/div[1]/div[3]/span[2]");
+
+        try {
+            Assert.assertEquals(TodayGenerateCommission,TodayGenerateCommissionjump);
+        }catch (AssertionError error){
+            System.out.println("今日佣金总计金额不一致");
+        }
+
+        driverUtil3.LoginAfter();
+    }
+
 }
